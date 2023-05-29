511e98ecbec617b2b9e4d40e98e13d100c6d8064
Andrey Kniazev <worldpwn@hotmail.com>
2023-05-29 16:22:13 +0200

    Added description

--- src/VoucherSystem/Program.cs
+++ src/VoucherSystem/Program.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections.Generic;
 using VoucherSystem.Dtos;
 using VoucherSystem.Generator;
+using Swashbuckle.AspNetCore.Annotations;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(o => o.EnableAnnotations());
 
 builder.Services.AddSingleton<IGenerateRandomSymbol, GenerateRandomSymbol>();
 builder.Services.AddScoped<GenerateVoucher>();
@@ -19,13 +20,14 @@ app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
-app.MapGet("generate-vouchers", (int voucherLength, int numberOfVouchersNeeded, GenerateVoucher generateVoucher) =>
+app.MapGet("generate-vouchers", [SwaggerOperation(
+        Summary = "Generate random `vouchers`.",
+        Description = "Will generate random `vouchers` based on the needed length of the `voucher` and the number of `vouchers` needed.")] (int voucherLength, int numberOfVouchersNeeded, GenerateVoucher generateVoucher) =>
 {
     HashSet<string> generatedVouchers = generateVoucher.GenerateRandomUniqueVouchers(voucherLength, numberOfVouchersNeeded);
     Vouchers vouchers = new Vouchers(string.Join(",", generatedVouchers), numberOfVouchersNeeded);
     return vouchers;
 })
-.WithName("GenerateVouchers")
 .WithOpenApi();
 
 app.Run();
