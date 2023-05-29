4f769b882fd52b34e327ae4e5cbf7c5e6f7b138b
Andrey Kniazev <worldpwn@hotmail.com>
2023-05-29 15:07:30 +0200

    Add integration test to get voucher

--- src/VoucherSystem.TestsIntegration/GenerateVouchers.cs
+++ src/VoucherSystem.TestsIntegration/GenerateVouchers.cs
@@ -1,11 +1,39 @@
-﻿namespace VoucherSystem.TestsIntegration;
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.RegularExpressions;
+using VoucherSystem.Dtos;
+
+namespace VoucherSystem.TestsIntegration;
 
 public class GenerateVouchers
 {
+    Regex vaucherRegex = new("^[A-Z0-9]+$");
+
     [Fact]
-    public void ShouldGenerateVouchers()
+    public async Task ShouldGenerateVouchers()
     {
-        var urlToWebApi = Environment.GetEnvironmentVariable("URL_TO_WEB_API");
-        Assert.Equal("LOL", urlToWebApi);
+        int voucherLength = 12;
+        int numberOfVouchersNeeded = 100;
+    
+
+        string? urlToWebApiFromEnv = Environment.GetEnvironmentVariable("URL_TO_WEB_API");
+        Uri urlToWebApi = new Uri(urlToWebApiFromEnv!);
+
+        HttpClient client = new();
+        client.BaseAddress = urlToWebApi;
+
+        Vouchers? response = await client.GetFromJsonAsync<Vouchers>($"generate-vouchers?voucherLength={voucherLength}&numberOfVouchersNeeded=${numberOfVouchersNeeded}");
+
+        Assert.NotNull(response);
+        Assert.Equal(numberOfVouchersNeeded, response.amount);
+
+        // validate vouchers
+        string[] vouchersItslef = response.vouchers.Split(',');
+        Assert.Equal(numberOfVouchersNeeded, vouchersItslef.Length);
+        foreach (string voucher in vouchersItslef)
+        {
+            Assert.Equal(voucherLength, voucher.Length);
+            Assert.True(vaucherRegex.Match(voucher).Success);
+        }
     }
 }
---
+++ src/VoucherSystem/Dtos/Vouchers.cs
@@ -0,0 +1,5 @@
+﻿using System;
+namespace VoucherSystem.Dtos;
+
+public record Vouchers(string vouchers, int amount);
+
