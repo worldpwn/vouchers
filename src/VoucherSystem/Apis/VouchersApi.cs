eb2d69b76371b1122acd5f09ad9101edfef9f237
Andrey Kniazev <worldpwn@hotmail.com>
2023-05-29 17:29:50 +0200

    Move voucher logic into VouchersApi

---
+++ src/VoucherSystem/Apis/VouchersApi.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using VoucherSystem.Dtos;
+using VoucherSystem.Generator;
+using VoucherSystem.Store;
+
+namespace VoucherSystem.Apis;
+
+public class VouchersApi
+{
+	private readonly GenerateVoucher generateVoucher;
+    private readonly AzureStorageTable azureStorageTable;
+
+    public VouchersApi(GenerateVoucher generateVoucher, AzureStorageTable azureStorageTable)
+    {
+        this.generateVoucher = generateVoucher;
+        this.azureStorageTable = azureStorageTable;
+    }
+
+    public async Task<Vouchers> GenerateRandomUniqueVouchers(int voucherLength, int numberOfVouchersNeeded)
+	{
+        HashSet<string> generatedVouchers = generateVoucher.GenerateRandomUniqueVouchers(voucherLength, numberOfVouchersNeeded);
+        Vouchers vouchers = new Vouchers(string.Join(",", generatedVouchers), numberOfVouchersNeeded);
+        return vouchers;
+    }
+}
+
--- src/VoucherSystem/Program.cs
+++ src/VoucherSystem/Program.cs
@@ -2,6 +2,8 @@
 using VoucherSystem.Dtos;
 using VoucherSystem.Generator;
 using Swashbuckle.AspNetCore.Annotations;
+using VoucherSystem.Apis;
+using VoucherSystem.Store;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +14,8 @@ builder.Services.AddSwaggerGen(o => o.EnableAnnotations());
 
 builder.Services.AddSingleton<IGenerateRandomSymbol, GenerateRandomSymbol>();
 builder.Services.AddScoped<GenerateVoucher>();
+builder.Services.AddScoped<VouchersApi>();
+builder.Services.AddSingleton<AzureStorageTable>();
 
 var app = builder.Build();
 
@@ -21,15 +25,11 @@ app.UseSwaggerUI();
 app.UseHttpsRedirection();
 
 app.MapGet("generate-vouchers",
-[SwaggerOperation(
+    [SwaggerOperation(
     Summary = "Generate random `vouchers`.",
-    Description = "Will generate random `vouchers` based on the needed length of the `voucher` and the number of `vouchers` needed.")]
-(int voucherLength, int numberOfVouchersNeeded, GenerateVoucher generateVoucher) =>
-{
-    HashSet<string> generatedVouchers = generateVoucher.GenerateRandomUniqueVouchers(voucherLength, numberOfVouchersNeeded);
-    Vouchers vouchers = new Vouchers(string.Join(",", generatedVouchers), numberOfVouchersNeeded);
-    return vouchers;
-})
+    Description = "Will generate random `vouchers` based on the needed length of the `voucher` and the number of `vouchers` needed. For `voucherLength` recommended value is 8+.")]
+async (int voucherLength, int numberOfVouchersNeeded, VouchersApi vouchersApi)
+=> await vouchersApi.GenerateRandomUniqueVouchers(voucherLength: voucherLength, numberOfVouchersNeeded: numberOfVouchersNeeded))
 .WithOpenApi();
 
 app.Run();
