a077a21dfb5b07c67a410f296ed1085062d42fd1
Andrey Kniazev <worldpwn@hotmail.com>
2023-05-29 17:53:30 +0200

    Introduce MarketingCampaignName as Value Object

---
+++ src/VoucherSystem.TestsUnit/ValueObjectsTests/MarketingCampaignNameTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using VoucherSystem.ValueObjects;
+
+namespace VoucherSystem.TestsUnit.ValueObjectsTests;
+
+public class MarketingCampaignNameTests
+{
+    public class UrlName_ToString
+    {
+        [Theory]
+        [InlineData("summerHolidaysOffer")]
+        [InlineData("saleForPartner")]
+        [InlineData("sale2023")]
+        [InlineData("2023sale")]
+        public void Correct_Data_Should_Return_Value(string value)
+        {
+            MarketingCampaignName marketingCampaignName = new MarketingCampaignName() { Value = value};
+            Assert.Equal(value, marketingCampaignName.ToString());
+        }
+    }
+}
+
---
+++ src/VoucherSystem/ValueObjects/MarketingCampaignName.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace VoucherSystem.ValueObjects
+{
+	public record MarketingCampaignName()
+	{
+        private string _value = string.Empty;
+        public required string Value {
+            get => _value;
+            set
+            {
+                Regex regex = new("^[a-zA-Z\\d]+$");
+                bool isMatch = regex.IsMatch(value);
+                if (!isMatch) throw new ArgumentException("You can use only small letters and numbers.");
+                this._value = value;
+            }
+        }
+
+        public override string ToString() => this.Value;
+    }
+}
+
