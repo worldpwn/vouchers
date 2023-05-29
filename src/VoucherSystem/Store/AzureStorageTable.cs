734727d7ce3a28b4e9df7860bdc7edebc911921d
Andrey Kniazev <worldpwn@hotmail.com>
2023-05-29 20:11:46 +0200

    Remove key (key was regenerated) and return Azure Ad way

--- src/VoucherSystem/Store/AzureStorageTable.cs
+++ src/VoucherSystem/Store/AzureStorageTable.cs
@@ -18,15 +18,10 @@ public class AzureStorageTable
     /// </summary>
     public async Task<TableClient> GetTableClient(string tableName)
     {
-        string connectionString = "DefaultEndpointsProtocol=https;AccountName=vouchersyst;AccountKey=Qk3EMQyPfvOi8U5G55YgUEzSMRt0XYW7uvQ66Ezc7AId7VUDCoVxFiijzSxcQLAffwx8e5/L1bT6+AStd49eTw==;EndpointSuffix=core.windows.net";
         TableClient tableClient = new TableClient(
-                       connectionString,
-                       tableName);
-
-        //TableClient tableClient = new TableClient(
-        //                new Uri($"https://{storageAccountName}.table.core.windows.net"),
-        //                tableName,
-        //                new DefaultAzureCredential());
+                        new Uri($"https://{storageAccountName}.table.core.windows.net"),
+                        tableName,
+                        new DefaultAzureCredential());
         await tableClient.CreateIfNotExistsAsync();
         return tableClient;
     }
