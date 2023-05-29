8c4978f70e5d87430d5c04c6a504d0281f30384e
Andrey Kniazev <worldpwn@hotmail.com>
2023-05-29 16:02:44 +0200

    Add swagger for all env

--- src/VoucherSystem/Program.cs
+++ src/VoucherSystem/Program.cs
@@ -14,12 +14,8 @@ builder.Services.AddScoped<GenerateVoucher>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
