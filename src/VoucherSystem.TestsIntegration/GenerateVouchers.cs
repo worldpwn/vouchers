198f60405eee42584ea3061d36ef121bd37ed44f
Andrey Kniazev <worldpwn@hotmail.com>
2023-05-29 14:42:57 +0200

    Add integration test step with env variable

---
+++ src/VoucherSystem.TestsIntegration/GenerateVouchers.cs
@@ -0,0 +1,11 @@
+﻿namespace VoucherSystem.TestsIntegration;
+
+public class GenerateVouchers
+{
+    [Fact]
+    public void ShouldGenerateVouchers()
+    {
+        var urlToWebApi = Environment.GetEnvironmentVariable("URL_TO_WEB_API");
+        Assert.Equal("LOL", urlToWebApi);
+    }
+}
