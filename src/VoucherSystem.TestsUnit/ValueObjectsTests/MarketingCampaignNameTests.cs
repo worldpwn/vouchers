bb960e4ff6a49624c9c512ae58e2df65a4ee0e7c
Andrey Kniazev <worldpwn@hotmail.com>
2023-05-29 18:50:28 +0200

    Add to table name for marketing campaign

--- src/VoucherSystem.TestsUnit/ValueObjectsTests/MarketingCampaignNameTests.cs
+++ src/VoucherSystem.TestsUnit/ValueObjectsTests/MarketingCampaignNameTests.cs
@@ -12,10 +12,34 @@ public class MarketingCampaignNameTests
         [InlineData("saleForPartner")]
         [InlineData("sale2023")]
         [InlineData("2023sale")]
-        public void Correct_Data_Should_Return_Value(string value)
+        public void CorrectDataShouldReturnValue(string value)
         {
             MarketingCampaignName marketingCampaignName = new MarketingCampaignName() { Value = value};
             Assert.Equal(value, marketingCampaignName.ToString());
+        }
+
+        [Theory]
+        [InlineData("summer_Holidays_Offer")]
+        [InlineData("sale-for-partner")]
+        [InlineData("@")]
+        [InlineData("!")]
+        [InlineData("?")]
+        [InlineData("[")]
+        public void InCorrectDataShouldReturnThrow(string value)
+        {
+            Action action = () => new MarketingCampaignName() { Value = value };
+            Assert.Throws<ArgumentException>(action);
+        }
+
+        [Theory]
+        [InlineData("summerHolidaysOffer", "t_summerHolidaysOffer")]
+        [InlineData("saleForPartner", "t_saleForPartner")]
+        [InlineData("sale2023", "t_sale2023")]
+        [InlineData("2023sale", "t_2023sale")]
+        public void CorrectDataShouldTransformToTableName(string value, string asTableName)
+        {
+            MarketingCampaignName marketingCampaignName = new MarketingCampaignName() { Value = value };
+            Assert.Equal(asTableName, marketingCampaignName.ToTableName());
         }
     }
 }
--- src/VoucherSystem/ValueObjects/MarketingCampaignName.cs
+++ src/VoucherSystem/ValueObjects/MarketingCampaignName.cs
@@ -16,6 +16,7 @@ namespace VoucherSystem.ValueObjects
                 this._value = value;
             }
         }
+        public string ToTableName() => $"t_{Value}";
 
         public override string ToString() => this.Value;
     }
