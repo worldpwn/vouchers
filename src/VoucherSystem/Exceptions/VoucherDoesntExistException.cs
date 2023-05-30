9bc0f98a3b0aa9282aae74ce38c817b64df7e85b
Andrey Kniazev <worldpwn@hotmail.com>
2023-05-30 11:32:45 +0200

    Introduce VoucherDoesntExistException

---
+++ src/VoucherSystem/Exceptions/VoucherDoesntExistException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace VoucherSystem.Exceptions;
+
+public class VoucherDoesntExistException : Exception
+{
+	public VoucherDoesntExistException(string voucher) : base($"Voucher {voucher} doesn't exist")
+	{
+	}
+}
+
